b6973fd712bc969bbfe8c98c63565a6868f65802
Abishek kuamr <66769099+Abishekkumar123@users.noreply.github.com>
2021-02-08 23:57:14 -0600

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
-account_sid = "Your Account Sid"
-auth_token = "Your Auth Token"
-my_cell = "+Your Cell Number"
-my_twilio = "+Your Twilio Number"
+account_sid = "AC1280e8d1a7c5e706f1f6535fc9ce1fc1"
+auth_token = "fe97df05dc68433609bed34209991946"
+my_cell = "+9861721472"
+my_twilio = "+17014017340"
