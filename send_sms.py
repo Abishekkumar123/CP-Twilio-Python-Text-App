260a9003e284470637c978b6cd0adfe9d5fb1215
Abishek kuamr <66769099+Abishekkumar123@users.noreply.github.com>
2021-02-09 00:03:30 -0600

    Update send_sms.py

--- send_sms.py
+++ send_sms.py
@@ -4,7 +4,7 @@ from credentials import account_sid, auth_token, my_cell, my_twilio
 # Find these values at https://twilio.com/user/account
 client = TwilioRestClient(account_sid, auth_token)
 
-my_msg = "Your message goes here..."
+my_msg = "669575 is your SECRET one time password (OTP) for payment of Rs. 990.00 via GOOGLEPLAYMASTERM via Netbanking. Do not share it with anyone."
 
 message = client.messages.create(to=my_cell, from_=my_twilio,
                                      body=my_msg)
